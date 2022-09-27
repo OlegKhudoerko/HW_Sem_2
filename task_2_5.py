0bc9a5bc27d494b22dab79f835c285a9bf47a3f6
OlegKhudoerko <292oleg292@mail.ru>
2022-09-27 21:35:38 +0300

    task5_solution_end

--- task_2_5.py
+++ task_2_5.py
@@ -3,13 +3,13 @@
 # -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # -> [0, 7, 6, 3, 4, 2, 9, 5, 1, 8]
 
+import random
 N = 10
 data = []
 for i in range(N):
     data.append(i)
 print('-> ', data)
 
-import random
 for k in range(N):
     i = random.randint(0, N-1)
     temp = data[k]
